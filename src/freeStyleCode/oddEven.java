19eccb0359ba1936ca740dc1c8bab9a46637f409
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-07-30 01:05:57 +0530

    update Free Style code Branch

--- src/freeStyleCode/oddEven.java
+++ src/freeStyleCode/oddEven.java
@@ -14,7 +14,6 @@ boolean array where `true` means the number ends with an even digit, `false` oth
 
 public class oddEven {
     public static void main(String[] args) {
-
         int []arr = {2,4,5,6,7,8,9,66,77,88,99,9,87,65,43,23,45};
         System.out.println(Arrays.toString(oddEvenDigit(arr)));
     }
@@ -22,7 +21,6 @@ public class oddEven {
     public static boolean [] oddEvenDigit(int [] arr ){
         int length = arr.length;
         boolean[] odEven = new boolean[length];
-
         for (int i = 0; i < arr.length; i++){
             if(arr[i]%2 == 0){
                 odEven[i] = true;
@@ -30,7 +28,6 @@ public class oddEven {
                 odEven[i] = false;
             }
         }
-
         return odEven;
 
     }
