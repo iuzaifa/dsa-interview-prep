5db6e610c66b16c60307849a8a872e8472946afe
Abu Huzaifa <abuhuzaifaw7@gmial.com>
2025-07-29 21:19:03 +0530

    update leetcode

---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+
+
+        int x = 2;
+
+        boolean isTrue = (x == 4) ? true : false;
+
+        System.out.println(isTrue);
+    }
+
+}
