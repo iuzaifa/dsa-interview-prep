1dc4b96be908ed882c128e357308a057c6ac52b4
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-08-10 23:20:48 +0530

    Basic Problems Check Even or Odd GeekForGeeks

--- src/geeksforgeeks/basicProblems/Check_Even_or_Odd.java
+++ src/geeksforgeeks/basicProblems/Check_Even_or_Odd.java
@@ -6,10 +6,13 @@ public class Check_Even_or_Odd {
     public static void main(String[] args) {
 
 //        System.out.println(even_or_Odd(45));
-        System.out.println(Arrays.toString(even_or_Odd(new int[]{3,4,5,6,7,8,9})));
+//        System.out.println(Arrays.toString(even_or_Odd(new int[]{3,4,5,6,7,8,9})));
+//        System.out.println(Arrays.toString(total_CountEvenAndOdd(new int[]{3,4,5,6,7,8,9})));
+        System.out.println(Arrays.toString(oddEvenSort(new int[]{3,4,5,6,7,8,9})));
 
     }
 
+    // return String
     public static String even_or_Odd(int n) {
 
         if (n % 2 == 0){
@@ -19,6 +22,7 @@ public class Check_Even_or_Odd {
     }
 
 
+    // return String  Array
     public static String [] even_or_Odd(int []  numArr) {
         String [] oe = new String[numArr.length];
 
@@ -33,4 +37,46 @@ public class Check_Even_or_Odd {
         return oe;
     }
 
+    public static String  [] total_CountEvenAndOdd(int []  numArr) {
+
+        int countO = 0;
+        int countE = 0;
+
+        for (int i=0; i< numArr.length; i++){
+
+            if (numArr[i] % 2 == 0){
+                countE++;
+            }else {
+                countO++;
+            }
+
+        }
+
+        return new String []{"Total Even: "+countE, "Total Odd: "+countO};
+
+    }
+
+
+    public static  int[]  oddEvenSort(int []  numArr){
+
+        int [] sorted = new int[numArr.length];
+        int j = 0;
+        for (int i=0; i<numArr.length; i++){
+            if (numArr[i] % 2 == 0){
+                sorted[j] = numArr[i];
+                j++;
+            }
+        }
+
+        for (int k = 0; k < numArr.length; k++){
+            if (numArr[k] % 2 != 0){
+                sorted[j] = numArr[k];
+                j++;
+            }
+        }
+
+
+        return sorted;
+
+    }
 }
